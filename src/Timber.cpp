b82071112f91d2d3eebc81e1a28219eb5d99bf23
Kyrylo Minaiev <mrflyingchip@gmail.com>
2025-01-08 01:07:49 +0200

    Creating SFML window

--- src/Timber.cpp
+++ src/Timber.cpp
@@ -1,9 +1,14 @@
 // Timber.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <SFML\Graphics.hpp>
+
+using namespace sf;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    VideoMode videoMode(Vector2u(1920, 1080));
+    RenderWindow window(videoMode, "Timber", State::Fullscreen);
+
+    return 0;
 }
