847df614b9b8f520a3abfe47ae1c88c6f999f85c
Kyrylo Minaiev <mrflyingchip@gmail.com>
2025-01-08 13:38:02 +0200

    Added game entity class.

--- src/Timber.cpp
+++ src/Timber.cpp
@@ -2,19 +2,51 @@
 //
 
 #include <SFML\Graphics.hpp>
+#include <memory>
 
 using namespace sf;
+
+class GameEntity {
+private:
+    void UpdateSpritePosition() {
+        m_entitySprite->setPosition(m_position);
+    }
+
+    std::unique_ptr<Texture> m_spriteTexture;
+    std::unique_ptr<Sprite> m_entitySprite;
+    Vector2f m_position;
+public:
+    GameEntity(const std::filesystem::path& spriteName) : m_position(0, 0){
+        m_spriteTexture = std::make_unique<Texture>(spriteName);
+        m_entitySprite = std::make_unique<Sprite>(*m_spriteTexture.get());
+        UpdateSpritePosition();
+    }
+
+    ~GameEntity() = default;
+
+    void SetPosition(Vector2f position) {
+        m_position = position;
+        UpdateSpritePosition();
+    }
+
+    void Draw(RenderWindow& window) {
+        window.draw(*m_entitySprite.get());
+    }
+
+    void Move(Vector2f translate) {
+        SetPosition(m_position + translate);
+    }
+};
+
 
 int main()
 {
     VideoMode videoMode(Vector2u(1920, 1080));
     RenderWindow window(videoMode, "Timber", State::Fullscreen);
 
-    Texture backgroundTexture;
-    backgroundTexture.loadFromFile("res/graphics/background.png");
-
-    Sprite backgroundSprite(backgroundTexture);
-    backgroundSprite.setPosition(Vector2f(0, 0));
+    GameEntity background("res/graphics/background.png");
+    GameEntity tree("res/graphics/tree.png");
+    tree.SetPosition(Vector2f(810, 0));
 
     while (window.isOpen()) {
         if (Keyboard::isKeyPressed(Keyboard::Key::Escape)) {
@@ -22,7 +54,8 @@ int main()
         }
 
         window.clear();
-        window.draw(backgroundSprite);
+        background.Draw(window);
+        tree.Draw(window);
         window.display();
     }
 
