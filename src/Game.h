323f5e88d3ceb0f9bfd2c02b7d5c2754a297e0fe
Kyrylo Minaiev <mrflyingchip@gmail.com>
2025-01-14 12:09:55 +0200

    Added game class

---
+++ src/Game.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include <SFML\Graphics.hpp>
+#include "GameEntity.h"
+#include <sstream>
+#include <vector>
+
+using namespace sf;
+
+class Game
+{
+public:
+    void Run(RenderWindow& window);
+private:
+    const float TREE_HORIZONTAL_POSITION = 810;
+    const float TREE_VERTICAL_POSITION = 0;
+
+    const float START_BEE_HORIZONTAL_POSITION = 0;
+    const float START_BEE_VERTICAL_POSITION = 800;
+
+    void updateBranches(int seed);
+
+    const int NUM_BRANCHES = 6;
+    std::vector<Sprite> branches;
+    enum class side {LEFT, RIGHT, NONE};
+    std::vector<side> branchPositions;
+};
