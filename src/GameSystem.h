beb4399295b074e2f351eec9aa74264c9fb21580
Kyrylo Minaiev <mrflyingchip@gmail.com>
2025-01-15 13:59:49 +0200

    Added base class for game systems

---
+++ src/GameSystem.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <SFML/System/Time.hpp>
+
+class EntitySystem;
+
+class GameSystem
+{
+public:
+    GameSystem(EntitySystem* entitySystem) : entitySystem(entitySystem)
+    {
+    }
+
+    virtual ~GameSystem() = default;
+    virtual void update(sf::Time& dt) = 0;
+
+protected:
+    EntitySystem* entitySystem;
+};
