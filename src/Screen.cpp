8a0abcd695ee6932a58b28d66114210f67be7308
Kyrylo Minaiev <mrflyingchip@gmail.com>
2025-01-15 14:00:11 +0200

    Added screen class for keeping some window data.

---
+++ src/Screen.cpp
@@ -0,0 +1,15 @@
+﻿#include "Screen.h"
+
+#include <SFML/Graphics/RenderWindow.hpp>
+
+Screen* Screen::s_instance;
+
+Screen::Screen(sf::RenderWindow* window) : m_window(window)
+{
+    s_instance = this;
+}
+
+sf::Vector2u Screen::getWindowSize()
+{
+    return s_instance->m_window->getSize();
+}
---
+++ src/Screen.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <SFML/System/Vector2.hpp>
+
+namespace sf
+{
+    class RenderWindow;
+}
+
+class Screen
+{
+public:
+    Screen(sf::RenderWindow* window);
+    static sf::Vector2u getWindowSize();
+    
+private:
+    static Screen* s_instance;
+    sf::RenderWindow* m_window;
+};
+
+
