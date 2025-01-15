a03797e68fc654b0d64581aa1cfeaea64ca2840a
Kyrylo Minaiev <mrflyingchip@gmail.com>
2025-01-15 13:59:10 +0200

    Added entity system

---
+++ src/EntitySystem.cpp
@@ -0,0 +1,31 @@
+﻿#include "EntitySystem.h"
+#include "GameEntity.h"
+#include <SFML\Graphics.hpp>
+
+EntitySystem::EntitySystem(sf::RenderWindow* window) : m_renderWindow(window)
+{
+}
+
+void EntitySystem::drawEntities() const
+{
+    for (auto& entity : m_entities)
+    {
+        if(entity->isActive())
+        {
+            m_renderWindow->draw(*entity->getEntitySprite());
+        }
+    }
+}
+
+GameEntity* EntitySystem::createEntity(const std::filesystem::path& spriteName, sf::Vector2f position)
+{
+    std::unique_ptr<GameEntity> entity = std::make_unique<GameEntity>(spriteName, position);
+    GameEntity* ptr = entity.get();
+    m_entities.push_back(std::move(entity));
+    return ptr;
+}
+
+GameEntity* EntitySystem::createEntity(const std::filesystem::path& spriteName)
+{
+    return createEntity(spriteName, sf::Vector2f(0, 0));
+}
---
+++ src/EntitySystem.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <filesystem>
+#include <SFML/System/Vector2.hpp>
+#include <memory>
+#include <vector>
+#include "GameEntity.h"
+
+namespace sf
+{
+    class RenderWindow;
+}
+
+class EntitySystem
+{
+public:
+    EntitySystem(sf::RenderWindow* window);
+    void drawEntities() const;
+    GameEntity* createEntity(const std::filesystem::path& spriteName, sf::Vector2f position);
+    GameEntity* createEntity(const std::filesystem::path& spriteName);
+private:
+    std::vector<std::unique_ptr<GameEntity>> m_entities;
+    sf::RenderWindow* m_renderWindow;
+};
