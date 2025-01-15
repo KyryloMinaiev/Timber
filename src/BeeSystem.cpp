4a010a7d89e714fb00f20f426981fb2d55d5b920
Kyrylo Minaiev <mrflyingchip@gmail.com>
2025-01-15 14:00:30 +0200

    Added Bee system

---
+++ src/BeeSystem.cpp
@@ -0,0 +1,38 @@
+﻿#include "BeeSystem.h"
+
+#include "Screen.h"
+#include "EntitySystem.h"
+
+BeeSystem::BeeSystem(EntitySystem* entitySystem) : GameSystem(entitySystem), m_beeOusideScreen(true),
+                                                   m_beeSpeed(0)
+{
+    m_bee = entitySystem->createEntity("res/graphics/bee.png",
+                               sf::Vector2f(START_BEE_HORIZONTAL_POSITION, START_BEE_VERTICAL_POSITION));
+}
+
+BeeSystem::~BeeSystem() = default;
+
+void BeeSystem::update(sf::Time& dt)
+{
+    auto screenSize = Screen::getWindowSize();
+    auto edgeOffset = screenSize.x * 0.1f;
+    
+    if (m_beeOusideScreen)
+    {
+        srand((int)time(0));
+        m_beeSpeed = (rand() % 200) + 500;
+        srand((int)time(0) * 10);
+        float height = (rand() % 500) + 500;
+        m_bee->setPosition(sf::Vector2f(edgeOffset + screenSize.x, height));
+        m_beeOusideScreen = false;
+    }
+    else
+    {
+        m_bee->move(sf::Vector2f(-m_beeSpeed * dt.asSeconds(), 0));
+
+        if (m_bee->getPosition().x < -100)
+        {
+            m_beeOusideScreen = true;
+        }
+    }
+}
---
+++ src/BeeSystem.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "GameSystem.h"
+
+class GameEntity;
+
+class BeeSystem : public GameSystem
+{
+public:
+    BeeSystem(EntitySystem* entitySystem);
+    ~BeeSystem() override;
+
+    void update(sf::Time& dt) override;
+
+private:
+    const float START_BEE_HORIZONTAL_POSITION = 0;
+    const float START_BEE_VERTICAL_POSITION = 800;
+
+    GameEntity* m_bee;
+    bool m_beeOusideScreen;
+    float m_beeSpeed;
+};
