aff69b081b3eb0dcc1d1083c7720fd6e58d87a3a
Kyrylo Minaiev <mrflyingchip@gmail.com>
2025-01-20 00:00:22 +0200

    Added event manager

---
+++ src/EventManager.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <vector>
+
+enum class EventType
+{
+    GameStarted,
+    PlayerCollision,
+    PlayerMoved,
+    GameEnded,
+    TimeEnded
+};
+
+class IEventListener
+{
+public:
+    virtual ~IEventListener() = 0;
+    virtual void onEvent(EventType event) = 0;
+};
+
+class EventManager
+{
+public:
+    void invokeEvent(EventType event) const;
+    void addEventListener(IEventListener* eventListener);
+    void removeEventListener(const IEventListener* eventListener);
+private:
+    std::vector<IEventListener*> m_eventListeners;
+};
